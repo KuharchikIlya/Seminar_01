188a8f9bb490eebcbe873d614918790647368700
KuharchikIlya <svaps_srs@outlook.com>
2023-06-22 22:45:55 +0300

    first commit

---
+++ Task_01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input number => ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int squareNumber = number * number;
+
+Console.WriteLine(squareNumber);
+
+// Console.WriteLine($""Square {number} is {squareNumber}");
+
+// решение со степенью
+
+//Console.WriteLine("Input number => ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int power = 2; //степень числа
+// int squareNumber = (int)Math.Pow(number, power);
+// Console.WriteLine($""Square {number} is {squareNumber}");
---
+++ Task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.Write("Input number: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(n % 10);
